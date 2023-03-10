23c6ff713fabf058463120b2e8d6b4111bb86092
Sami Samara <samisamara99@yahoo.com>
2023-03-09 22:59:24 -0600

    started game details page

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
+const { application } = require('express');
 const express = require('express');
 const app = express();
 
 // connect to database
 const dbURI = "";
 
+// igdb.com auth tokens
+const client_id = "lxfuzmuilua8gb13ipcikeg8i65ou6";
+const client_secret = "ndwufpkw69w69wdd305caygwcr1v5x";
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+// post handler
+// app.post('/', (req, res) => {
+
+// })
+
+
+
 // register view engine
 app.set('view engine', 'ejs');
 app.listen(3000);
 
-app.use(express.static("public"));
+
 
 // routes
 app.get('/', (req, res) => {
   res.render('index', { title: "Home" });
+});
+
+app.get('/gameDetails', (req, res) => {
+  res.render('gameDetails', { title: "Game Details" })
 });
 
 app.use((req, res) => {
