ceeedf4726d1e93dee64ec16a8af8026a66c8395
Sami Samara <samisamara99@yahoo.com>
2023-03-21 23:59:15 -0500

    added additional dependencies

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const { application } = require('express');
 const express = require('express');
 const app = express();
+const apicalypse = require("apicalypse");
 
 // connect to database
 const dbURI = "";
 
 // igdb.com auth tokens
 const client_id = "lxfuzmuilua8gb13ipcikeg8i65ou6";
-const client_secret = "ndwufpkw69w69wdd305caygwcr1v5x";
+const client_secret = "lt1lt769d03a4cyh7ta43ovu5zuin8";
+
+const access_token = "kaudhxpfacfryczrbsp95s15uaqbe0";
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+// base URL
+const baseURL = "https://api.igdb.com/v4"
 
 
 // register view engine
 app.set('view engine', 'ejs');
 app.listen(3000);
 
-
+app.use((req, res, next) => {
+  console.log('log thing');
+  next();
+});
 
 // routes
 app.get('/', (req, res) => {
