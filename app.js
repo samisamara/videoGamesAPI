e8782a233e7301ec286b9609781ff15aa21bba05
Sami Samara <samisamara99@yahoo.com>
2023-04-04 01:45:31 -0500

    fixed api call to request data using the data (body) rather than through the url

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const { application, response } = require('express');
 const express = require('express');
 const app = express();
+const apicalypse = require("apicalypse")
 const axios = require("axios");
 
 // connect to database
@@ -37,26 +38,25 @@ app.post('/', (req, res) => {
   const searchterm = req.body.searchTerm;
   console.log("We searched for: " + searchterm);
   axios({
-    url: `https://api.igdb.com/v4/games?fields=id,name,summary,release_dates,platforms.name,cover.url,involved_companies.company.name,release_dates.date&limit=25&search=${searchterm}&where=cover.url!=null`,
+    url: 'https://api.igdb.com/v4/games',
     method: 'POST',
     headers: {
         'Accept': 'application/json',
         'Client-ID': client_id,
         'Authorization': `Bearer ${access_token}`,
     },
-    data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;"
+    data: `fields id,name,summary,release_dates,platforms.name,cover.url,involved_companies.company.name,release_dates.date; search "${searchterm}"; limit 50;`
   })
     .then(response => {
-
-        const collections = response.data;
-        collections.forEach(collection => {
-          collection.cover.url = collection.cover.url.replace('t_thumb', 't_1080p');
-        });
+      const collections = response.data;
+      collections.forEach(collection => {
+        collection.cover.url = collection.cover.url.replace('t_thumb', 't_1080p');
+      });
         
-        res.render('index', { title: "Home", searchterm, games: collections })
+      res.render('index', { title: "Home", searchterm, games: collections })
     })
     .catch(err => {
-        console.error(err);
+      console.error(err);
     });
 });
 
